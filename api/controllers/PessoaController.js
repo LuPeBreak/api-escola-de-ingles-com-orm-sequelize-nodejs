d2eead174550a32969bb749b7cb9ff83a831a52e
Luis Felipe de Paula Costa <lfbmrj15@gmail.com>
2022-04-03 10:47:18 -0300

    Finalizado CRUD Pessoas

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,23 +1,71 @@
-const database = require("../models");
+const { Pessoas } = require("../models");
 
 class PessoaController {
+  // lista todas as pessoas
   static async list(req, res) {
     try {
-      const todasAsPessoas = await database.Pessoas.findAll();
+      const todasAsPessoas = await Pessoas.findAll();
       return res.json(todasAsPessoas);
     } catch (err) {
       res.status(500).json(err.message);
     }
   }
+
+  // lista uma pessoa especifica pelo id
   static async find(req, res) {
     try {
       const { id } = req.params;
-      const pessoa = await database.Pessoas.findOne({
+      const pessoa = await Pessoas.findOne({
         where: {
-          id,
+          id: Number(id),
         },
       });
       res.json(pessoa);
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
+  }
+
+  // Cria uma pessoa
+  static async create(req, res) {
+    try {
+      const pessoa = req.body;
+      const pessoaCriada = await Pessoas.create(pessoa);
+      res.status(201).json(pessoaCriada);
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
+  }
+
+  // Atualiza uma pessoa especifica pelo id
+  static async update(req, res) {
+    try {
+      const { id } = req.params;
+      const dadosParaAtualizarPessoa = req.body;
+      await Pessoas.update(dadosParaAtualizarPessoa, {
+        where: { id: id },
+      });
+      const pessoaAtualizada = await Pessoas.findOne({
+        where: {
+          id: Number(id),
+        },
+      });
+      res.json(pessoaAtualizada);
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
+  }
+
+  // Deleta uma pessoa especifica pelo id
+  static async delete(req, res) {
+    try {
+      const { id } = req.params;
+      await Pessoas.destroy({
+        where: {
+          id: Number(id),
+        },
+      });
+      res.json({ message: "deletado com sucesso", id: id });
     } catch (err) {
       res.status(500).json(err.message);
     }
--- api/routes/pessoas.routes.js
+++ api/routes/pessoas.routes.js
@@ -4,6 +4,9 @@ const PessoaController = require("../controllers/PessoaController");
 PessoaRouter = Router();
 
 PessoaRouter.get("/", PessoaController.list);
+PessoaRouter.post("/", PessoaController.create);
 PessoaRouter.get("/:id", PessoaController.find);
+PessoaRouter.put("/:id", PessoaController.update);
+PessoaRouter.delete("/:id", PessoaController.delete);
 
 module.exports = PessoaRouter;
